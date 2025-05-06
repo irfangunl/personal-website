0bb3f97e874ddc59ea539ede52718f351da889fb
rfan Günel <irfangunel4@gmail.com>
2025-05-06 23:33:25 +0300

    Initial commit: React personal website start

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center',marginTop: '25px'}}>
+      <h1>It's a new start</h1>
+      <p>Under Development.</p>
+    </div>
+  );
+}
+export default App;
